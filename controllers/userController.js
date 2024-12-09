68569935273e4d2d4efafe3de0dc270b9a8d7084
Lauliek <laureline.pellegrini@gmail.com>
2024-12-09 10:49:37 +0100

    redirection vers page user apres login ou enregistrement

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,7 +58,7 @@ function showLogin(req, res) {
 }
 
  */
-// VERSION AVEC LE HACHAGE DES MDP
+/* // VERSION AVEC LE HACHAGE DES MDP
 function traiteLogin(req, res) {
     const { username, password } = req.body;
     db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
@@ -73,6 +73,25 @@ function traiteLogin(req, res) {
         }
     });
 }
+ */
+
+// VERSION AVEC LE HACHAGE DES MDP et la REDIRECTION VERS LA PAGE UTILISATEUR
+function traiteLogin(req, res) {
+    const { username, password } = req.body;
+    db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
+        if (err) {
+            console.error("Erreur lors de la vérification de l'utilisateur :", err.message);
+            return res.send('ERROR');
+        }
+        if (row && bcrypt.compareSync(password, row.password)) {
+            //res.send("Bienvenue");
+           return res.redirect('./user');  // pour rediriger l utilisateur vers la page user
+        } else {
+            return res.send(loginView("Error : Mot de passe ou nom d'utilisateur incorrect")); // ça laissera l'affichage du formulaire
+        }
+    });
+}
+
 
 
 
@@ -174,7 +193,7 @@ function showRegister(req, res) {
 }
  */
 
-// DANS CETTE VERSION ON VERIF LES DOUBLON UTILISATEUR ET ON HACHE LE MDP
+/* // DANS CETTE VERSION ON VERIF LES DOUBLON UTILISATEUR ET ON HACHE LE MDP
 function traiteRegister(req, res) {
     const { username, password } = req.body; // Récupére les données du formulaire
     if (!username || !password) {            // Vérifie si les champs sont remplis
@@ -211,6 +230,52 @@ function traiteRegister(req, res) {
                     } else {
                         console.log("user succes :" , newUser, " Utilisateur créé avec succès :", username);
                         return res.send("Compte créé avec succès."); // masquer si on le désire
+                    } 
+                });
+            }
+        });
+    }
+}
+ */
+
+// DANS CETTE VERSION ON VERIF LES DOUBLON UTILISATEUR ET ON HACHE LE MDP et REDIRECTION vers la page user apres enregistrement
+function traiteRegister(req, res) {
+    const { username, password } = req.body; // Récupére les données du formulaire
+    if (!username || !password) {            // Vérifie si les champs sont remplis
+        return res.send("Veuillez remplir tous les champs.");
+    } else {
+        const hashedPassword = bcrypt.hashSync(password, 10); // Hachage du mot de passe avec un salt de 10
+        const newUser = new User(username, hashedPassword);
+        const queryExist = `SELECT * FROM users WHERE username = ? `;    //vérifie si un utilisateur avec le même nom d'utilisateur existe.
+        db.get(queryExist, [username], (err, row) => {     // on vérifie uniquement le nom d utilisateur
+            //Explication de row : Dans le contexte de la méthode db.get, row représente la ligne de résultat retournée par la requête SQL. Si un utilisateur correspondant aux critères de la requête est trouvé, row contiendra les données de cet utilisateur. Sinon, row sera undefined.
+            if (err) {
+                console.error("Erreur lors de la vérification :", err.message);
+                return res.send("ERROR : Erreur interne du serveur.");
+            }
+            else if (row) {    // L'utilisateur existe déjà, 
+                console.log("Utilisateur trouvé :", row); // pour voir pb dans le code
+                if (bcrypt.compareSync(password, row.password)) {  // on vérifie le mot de passe
+                    console.log("Compte déjà existant, authentification requise");
+                    return res.send(`
+                        <p>Vous avez déjà un compte, veuillez vous authentifier.</p>
+                        <button onclick="window.location.href='/login'">Aller à la page de connexion</button>
+                    `);
+                } else {  // Le nom d'utilisateur existe mais le mot de passe est différent
+                    console.log("Nom d'utilisateur déjà pris.");
+                    return res.send(registerView("Nom d'utilisateur déjà existant, veuillez en choisir un autre.")); // ça laissera l'affichage du formulaire
+                }
+            } else {
+            // Insère le nouvel utilisateur
+                const queryInsert = `INSERT INTO users(username, password) VALUES (?, ?)`;
+                db.run(queryInsert, [newUser.username, newUser.password], function (err) {
+                    if (err) {
+                        console.error("register échoué : Erreur lors de l'enregistrement :", err.message);
+                        return res.send( 'ERROR : Erreur lors de la création du compte.');
+                    } else {
+                        console.log("user succes :" , newUser, " Utilisateur créé avec succès :", username);
+                        //return res.send("Compte créé avec succès."); // masquer si on le désire
+                        return res.redirect('./user');  // pour rediriger l utilisateur vers la page user
                     } 
                 });
             }
