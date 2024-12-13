cfc817898ea4bf6f2f979b85b1f85eb2fb407b77
Lauliek <laureline.pellegrini@gmail.com>
2024-12-13 12:29:39 +0100

    modif du middleware pour prise en charge du token avec role

--- middlewares/verifyTokenMiddleware.js
+++ middlewares/verifyTokenMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const secretKey = process.env.SECRET_KEY; // s'assurer d'avoir une clé secrète dans .env
 
-// Middleware pour vérifier le token
+/* // Middleware pour vérifier le token
 function verifyTokenMiddleware(req, res, next) {
     const token = req.cookies.token; // Récupè le token dans les cookies
     if (!token) {
@@ -19,6 +19,35 @@ function verifyTokenMiddleware(req, res, next) {
         next(); // Passe au middleware ou à la fonction suivante
     });
 }
+
+ */
+
+// middleware pour verifier le token avec role
+function verifyTokenMiddleware(roleRequired) {
+    return function(req, res, next) {
+        const token = req.cookies.token;   // Récupè le token dans les cookies
+        if (!token) {
+            return res.redirect('/login'); // Redirige si aucun token n'est trouvé
+        }
+        jwt.verify(token, secretKey, (err, decoded) => {
+            if (err) {
+                return res.redirect('/login');   // Redirige si le token est invalide
+            }
+
+            // Vérifie si l'utilisateur a le rôle requis
+            if (decoded.role !== roleRequired) {
+                return res.status(403).send("Accès refusé : Vous n'avez pas les droits nécessaires.");
+            }
+            // Ajoute les informations décodées à l'objet req
+            req.user = decoded;  // Ajoute les infos utilisateur à la requête, `decoded` contient les données encodées dans le token
+            next();  // Passe au middleware ou à la fonction suivante
+        });
+    };
+}
+
+
+
+
 
 
 module.exports = verifyTokenMiddleware
